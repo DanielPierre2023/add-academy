338a828eabbbcee417bc5b929e9b225ae8417a57
DanielPierre2023 <152337490+DanielPierre2023@users.noreply.github.com>
2026-08-05 17:22:53 +0200

    Enable SSR for LoginForm dynamic import

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,7 @@
+'use client';
+
 import dynamic from 'next/dynamic';
 
-// Dynamically import the client form with SSR disabled to prevent prerender errors
 const LoginForm = dynamic(() => import('./login-form'), { ssr: false });
 
 export default function LoginPage() {
